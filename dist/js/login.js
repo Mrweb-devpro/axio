a50ebab4d0f233bebb914a61f14d3a639bc42fe0
david nwokedike <miklileza@gmail.com>
2023-09-06 21:38:55 -0700

    added some js file

---
+++ dist/js/login.js
@@ -0,0 +1,7 @@
+"use strict";
+const loginEmail = document.querySelector(".loign-email-inp");
+const loginPasswordInp = document.querySelector(".login-password-inp");
+const loginBtn = document.querySelector(".login-btn");
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+});
